207164861fcb33de3cfa565e553ebfc9f8f92ce6
nisar4398 <nissarkhann4398@gmail.com>
2025-07-27 14:16:10 +0500

    first commit

---
+++ app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('', views.index, name= 'index'),
+    path('about/',views.about , name= 'about'),
+    path('courses/', views.courses, name= 'courses'),
+    path('trainers/',views.trainers, name= 'trainers'),
+    path('events/',views.events, name= 'events'),
+    path('pricing/', views.pricing, name= 'pricing'),
+    path('contact/',views.contact, name= 'contact'),
+]
---
+++ app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .import models
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request,'about.html')
+
+def courses(request):
+    return render(request,'courses.html')
+
+def trainers(request):
+    return render(request, 'trainers.html')
+
+def events(request):
+    return render(request,'events.html')
+
+def pricing(request):
+    return render(request,'pricing.html')
+
+def contact(request):
+    return render(request, 'contact.html')
